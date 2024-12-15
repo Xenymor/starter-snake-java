afc577a168c153087532a9bb3f2a5d6c8a4d22dd
Xenymor <101249409+Xenymor@users.noreply.github.com>
2024-12-15 16:20:29 +0100

    Test

--- src/main/java/com/battlesnake/starter/Snake.java
+++ src/main/java/com/battlesnake/starter/Snake.java
@@ -296,7 +296,6 @@ public class Snake {
             } else {
                 currFoodScore = FOOD_SCORE;
             }
-            currFoodScore = 0; //no 4200 TODO delete this
 
             for (Coord neighbor : neighbors) {
                 final int dist = foodDists[neighbor.x][neighbor.y];
